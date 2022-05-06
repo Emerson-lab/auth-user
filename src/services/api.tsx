fe53a019806607c65d34eb1ed5950de9c9b836db
Emerson Trindade <trinddade29@gmail.com>
2022-05-05 22:27:02 -0300

    type error correction

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,11 +1,45 @@
-import axios from "axios";
-import { parseCookies } from "nookies"
+import axios, {AxiosError} from "axios";
+import { parseCookies, setCookie } from "nookies"
 
-const cookies = parseCookies()
+let cookies = parseCookies()
+
+type AxiosErrorProps = {
+  code: string;
+}
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
   headers: {
     Authorization: `Bearer ${cookies['nextauth.token']}`
+  }
+});
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError<AxiosErrorProps>) => {
+  if(error.response?.status === 401) {
+    if(error.response?.data.code === 'token.expored'){
+       cookies = parseCookies();
+
+       const {'nextauth.refreshToken': refreshToken} = cookies;
+
+       api.post('/refresh', {
+         refreshToken,
+       }).then((response) => {
+         const {token} = response.data?.token;
+         setCookie(undefined, 'nextauth.token', token, {
+          maxAge: 60 * 60  * 1, // 1 hora
+        
+        })
+
+        setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60  * 1, // 1 hora
+        
+        })
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+       });
+    }else {
+      //deslogar o usuário
+    }
   }
 })
